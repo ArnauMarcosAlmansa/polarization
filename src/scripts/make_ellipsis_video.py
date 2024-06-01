d5928fe454f741ef80630618361039d8c28c1d31
Arnau Marcos Almansa <arnaumarcosalmansa@gmail.com>
2024-06-01 05:22:41 +0200

    removed ellipse center offset to simplify

--- src/scripts/make_ellipsis_video.py
+++ src/scripts/make_ellipsis_video.py
@@ -16,82 +16,51 @@ class PointRelationship(Enum):
     OUTSIDE = 2
 
 
-def g(a: float, b: float, A: float, h: float, k: float):
+def g(a: float, b: float, A: float):
     def f(x: float, y: float) -> float:
-        return (((h - x) * cos(A) + (k - y) * sin(A)) ** 2 / a ** 2 + (
-                    (k - y) * cos(A) - (h - x) * sin(A)) ** 2 / b ** 2 - 1) ** 2
+        return ((x * cos(A) + y * sin(A)) ** 2 / a ** 2 + (y * cos(A) - x * sin(A)) ** 2 / b ** 2 - 1) ** 2
 
     return f
 
 
-def diff_a(a: float, b: float, A: float, h: float, k: float) -> Callable[[float, float], float]:
+def diff_a(a: float, b: float, A: float) -> Callable[[float, float], float]:
     def grad_a(x: float, y: float) -> float:
-        return -4 * ((h - x) * cos(A) + (k - y) * sin(A)) ** 2 * (
-                ((h - x) * cos(A) + (k - y) * sin(A)) ** 2 / a ** 2 + (
-                (k - y) * cos(A) - (h - x) * sin(A)) ** 2 / b ** 2 - 1) / a ** 3
+        return -4 * (x * cos(A) + y * sin(A)) ** 2 * (
+                (x * cos(A) + y * sin(A)) ** 2 / a ** 2 + (y * cos(A) - x * sin(A)) ** 2 / b ** 2 - 1) / a ** 3
 
     return grad_a
 
 
-def diff_b(a: float, b: float, A: float, h: float, k: float) -> Callable[[float, float], float]:
+def diff_b(a: float, b: float, A: float) -> Callable[[float, float], float]:
     def grad_b(x: float, y: float) -> float:
-        return -4 * ((k - y) * cos(A) - (h - x) * sin(A)) ** 2 * (
-                ((h - x) * cos(A) + (k - y) * sin(A)) ** 2 / a ** 2 + (
-                (k - y) * cos(A) - (h - x) * sin(A)) ** 2 / b ** 2 - 1) / b ** 3
+        return -4 * (y * cos(A) - x * sin(A)) ** 2 * (
+                (x * cos(A) + y * sin(A)) ** 2 / a ** 2 + (y * cos(A) - x * sin(A)) ** 2 / b ** 2 - 1) / b ** 3
 
     return grad_b
 
 
-def diff_A(a: float, b: float, A: float, h: float, k: float) -> Callable[[float, float], float]:
+def diff_A(a: float, b: float, A: float) -> Callable[[float, float], float]:
     def grad_A(x: float, y: float) -> float:
-        return 4 * (((h - x) * cos(A) + (k - y) * sin(A)) ** 2 / a ** 2 + (
-                (k - y) * cos(A) - (h - x) * sin(A)) ** 2 / b ** 2 - 1) * (
-                ((h - x) * cos(A) + (k - y) * sin(A)) * ((k - y) * cos(A) - (h - x) * sin(A)) / a ** 2 - (
-                (h - x) * cos(A) + (k - y) * sin(A)) * ((k - y) * cos(A) - (h - x) * sin(A)) / b ** 2)
+        return 4 * ((x * cos(A) + y * sin(A)) ** 2 / a ** 2 + (y * cos(A) - x * sin(A)) ** 2 / b ** 2 - 1) * (
+                (x * cos(A) + y * sin(A)) * (y * cos(A) - x * sin(A)) / a ** 2 - (x * cos(A) + y * sin(A)) * (
+                y * cos(A) - x * sin(A)) / b ** 2)
 
     return grad_A
 
 
-def diff_h(a: float, b: float, A: float, h: float, k: float) -> Callable[[float, float], float]:
-    def grad_h(x: float, y: float) -> float:
-        return 4 * (((h - x) * cos(A) + (k - y) * sin(A)) ** 2 / a ** 2 + (
-                (k - y) * cos(A) - (h - x) * sin(A)) ** 2 / b ** 2 - 1) * (
-                ((h - x) * cos(A) + (k - y) * sin(A)) * cos(A) / a ** 2 - (
-                (k - y) * cos(A) - (h - x) * sin(A)) * sin(A) / b ** 2)
-
-    return grad_h
-
-
-def diff_k(a: float, b: float, A: float, h: float, k: float) -> Callable[[float, float], float]:
-    def grad_k(x: float, y: float) -> float:
-        return 4 * (((h - x) * cos(A) + (k - y) * sin(A)) ** 2 / a ** 2 + (
-                (k - y) * cos(A) - (h - x) * sin(A)) ** 2 / b ** 2 - 1) * (
-                ((k - y) * cos(A) - (h - x) * sin(A)) * cos(A) / b ** 2 + (
-                (h - x) * cos(A) + (k - y) * sin(A)) * sin(A) / a ** 2)
-
-    return grad_k
-
-
 class Ellipse:
-    h: float
-    k: float
     A: float
     a: float
     b: float
 
-    def __init__(self, h: float, k: float, A: float, a: float, b: float):
-        self.h = h
-        self.k = k
-        self.A = A
+    def __init__(self, a: float, b: float, A: float):
         self.a = a
         self.b = b
+        self.A = A
 
     def _compute_right_term(self, x: float, y: float) -> float:
-        cx = x - self.h
-        cy = y - self.k
-
-        u1 = ((cx * cos(self.A)) + (cy * sin(self.A))) ** 2
-        u2 = ((cx * sin(self.A)) - (cy * cos(self.A))) ** 2
+        u1 = ((x * cos(self.A)) + (y * sin(self.A))) ** 2
+        u2 = ((x * sin(self.A)) - (y * cos(self.A))) ** 2
         d1 = self.a ** 2
         d2 = self.b ** 2
 
@@ -113,31 +82,15 @@ class Ellipse:
         a = self.a
         b = self.b
         A = self.A
-        h = self.h
-        k = self.k
 
         y1 = -(sqrt(a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2 - (
-                cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * h ** 2 + 2 * (
-                            cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * h * x - (
-                            cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * x ** 2) * a * b + (
-                       a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A)) * h - (
-                       a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2) * k - (
+                cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * x ** 2) * a * b - (
                        a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A)) * x) / (
                      a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2)
 
         y2 = (sqrt(a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2 - (
-                cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * h ** 2 + 2 * (
-                           cos(A) ** 4 + 2 * cos(A) ** 2 * sin(
-                       A) ** 2 + sin(A) ** 4) * h * x - (
-                           cos(A) ** 4 + 2 * cos(A) ** 2 * sin(
-                       A) ** 2 + sin(
-                       A) ** 4) * x ** 2) * a * b - (
-                      a ** 2 * cos(A) * sin(A) - b ** 2 * cos(
-                  A) * sin(A)) * h + (
-                      a ** 2 * cos(A) ** 2 + b ** 2 * sin(
-                  A) ** 2) * k + (
-                      a ** 2 * cos(A) * sin(A) - b ** 2 * cos(
-                  A) * sin(A)) * x) / (
+                cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * x ** 2) * a * b + (
+                      a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A)) * x) / (
                      a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2)
         return y1, y2
 
@@ -164,78 +117,35 @@ class Ellipse:
         a = self.a
         b = self.b
         A = self.A
-        h = self.h
-        k = self.k
 
-        x = -(sqrt(-cos(A) ** 4 - 2 * cos(A) ** 2 * sin(A) ** 2 - sin(A) ** 4 + 2 * (
-                    cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * h * cos(theta) + (
-                               a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2 - (
-                                   cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * h ** 2) * cos(
-            theta) ** 2 + (b ** 2 * cos(A) ** 2 + a ** 2 * sin(A) ** 2 - (
-                    cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * k ** 2) * sin(theta) ** 2 + 2 * (
-                               (cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * k + (
-                                   a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A) - (
-                                       cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * h * k) * cos(
-                           theta)) * sin(theta)) * a * b * sin(theta) - (
-                          (b ** 2 * cos(A) ** 2 + a ** 2 * sin(A) ** 2) * h - (
-                              a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A)) * k) * sin(theta) ** 2 - (
+        x = -(sqrt(-cos(A) ** 4 - 2 * cos(A) ** 2 * sin(A) ** 2 - sin(A) ** 4 + (
+                    a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2) * cos(theta) ** 2 + 2 * (
+                               a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A)) * cos(theta) * sin(theta) + (
+                               b ** 2 * cos(A) ** 2 + a ** 2 * sin(A) ** 2) * sin(theta) ** 2) * a * b * sin(theta) - (
                           a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2) * cos(theta) - (
-                          a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A) + (
-                              (a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A)) * h - (
-                                  a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2) * k) * cos(theta)) * sin(theta)) / (
+                          a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A)) * sin(theta)) / (
                         (a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2) * cos(theta) ** 2 + 2 * (
                             a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A)) * cos(theta) * sin(theta) + (
                                     b ** 2 * cos(A) ** 2 + a ** 2 * sin(A) ** 2) * sin(theta) ** 2)
-        y = (sqrt(-cos(A) ** 4 - 2 * cos(A) ** 2 * sin(A) ** 2 - sin(A) ** 4 + 2 * (
-                    cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * h * cos(theta) + (
-                              a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2 - (
-                                  cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * h ** 2) * cos(
-            theta) ** 2 + (b ** 2 * cos(A) ** 2 + a ** 2 * sin(A) ** 2 - (
-                    cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * k ** 2) * sin(theta) ** 2 + 2 * (
-                              (cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * k + (
-                                  a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A) - (
-                                      cos(A) ** 4 + 2 * cos(A) ** 2 * sin(A) ** 2 + sin(A) ** 4) * h * k) * cos(
-                          theta)) * sin(theta)) * a * b * cos(theta) - (
-                         (a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A)) * h - (
-                             a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2) * k) * cos(theta) ** 2 + (
+        y = (sqrt(-cos(A) ** 4 - 2 * cos(A) ** 2 * sin(A) ** 2 - sin(A) ** 4 + (
+                    a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2) * cos(theta) ** 2 + 2 * (
+                              a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A)) * cos(theta) * sin(theta) + (
+                              b ** 2 * cos(A) ** 2 + a ** 2 * sin(A) ** 2) * sin(theta) ** 2) * a * b * cos(theta) + (
                          a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A)) * cos(theta) + (
-                         b ** 2 * cos(A) ** 2 + a ** 2 * sin(A) ** 2 - (
-                             (b ** 2 * cos(A) ** 2 + a ** 2 * sin(A) ** 2) * h - (
-                                 a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A)) * k) * cos(theta)) * sin(
-            theta)) / ((a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2) * cos(theta) ** 2 + 2 * (
-                    a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A)) * cos(theta) * sin(theta) + (
-                                   b ** 2 * cos(A) ** 2 + a ** 2 * sin(A) ** 2) * sin(theta) ** 2)
-
-        return sqrt(x**2 + y ** 2)
+                         b ** 2 * cos(A) ** 2 + a ** 2 * sin(A) ** 2) * sin(theta)) / (
+                        (a ** 2 * cos(A) ** 2 + b ** 2 * sin(A) ** 2) * cos(theta) ** 2 + 2 * (
+                            a ** 2 * cos(A) * sin(A) - b ** 2 * cos(A) * sin(A)) * cos(theta) * sin(theta) + (
+                                    b ** 2 * cos(A) ** 2 + a ** 2 * sin(A) ** 2) * sin(theta) ** 2)
+        return sqrt(x ** 2 + y ** 2)
 
     def __str__(self):
-        return f"Ellipse(a={self.a}, b={self.b}, A={self.A}, h={self.h}, k={self.k})"
-
-
-def ellipse_from_gradient_descent(points: list[tuple[float, float]]) -> Ellipse:
-    state = {"a": 1, "b": 1, "A": 0, "h": 0, "k": 0}
-    # state = {"a": 5, "b": 1, "A": 0, "h": 0, "k": 1}
-    lr = 0.005
-    for _ in range(500000):
-        point = random.choice(points)
-        grads = [
-            diff_a(**state),
-            diff_b(**state),
-            diff_A(**state),
-            diff_h(**state),
-            diff_k(**state),
-        ]
-        for grad, v in zip(grads, ["a", "b", "A", "h", "k"]):
-            state[v] -= grad(point[0], point[1]) * lr
-
-    return Ellipse(**state)
-
+        return f"Ellipse(a={self.a}, b={self.b}, A={self.A})"
 
 def centered_ellipse_from_gradient_descent(points: list[tuple[float, float]]) -> Ellipse:
-    state = {"a": 1, "b": 1, "A": 0, "h": 0, "k": 0}
+    state = {"a": 1, "b": 1, "A": 0}
     # state = dict(a=1.4106914984369574 * 2, b=0.09974716155205582 * 2, A=0.7803478301047874 * 2, h=0, k=0)
-    lr = 10
-    for i in range(5000000):
+    lr = 0.005
+    for i in range(50000):
         if i % 10 == 0:
             cost = 0
             for p in points:
@@ -266,11 +176,10 @@ def ellipse_from_stokes_vector(stokes_vector: StokesVector) -> Ellipse:
     a = sqrt(s0 * (1 + p))
     b = sqrt(s0 * (1 - p))
 
-    return Ellipse(0, 0, A, a, b)
+    return Ellipse(a, b, A)
 
 
 def make_elipse_image(I0: np.ndarray, I45: np.ndarray, I90: np.ndarray, I135: np.ndarray) -> list[list[Ellipse]]:
-
     image = []
     for i in range(I0.shape[0]):
         row = []
@@ -295,10 +204,7 @@ def render_ellipse(image: list[list[Ellipse]], angle: float) -> np.ndarray:
     for i in range(len(image)):
         row = []
         for j in range(len(image[0])):
-            try:
-                row.append(image[i][j].intensity(angle))
-            except ValueError:
-                row.append(0)
+            row.append(image[i][j].intensity(angle))
         rendered.append(row)
 
     im = np.matrix(rendered)
@@ -306,40 +212,38 @@ def render_ellipse(image: list[list[Ellipse]], angle: float) -> np.ndarray:
 
 
 if __name__ == '__main__':
-    e = Ellipse(0, 1, 0, 5, 2)
-    print(e.point_related_to_ellipsis(5, 1))
 
     # e = ellipse_from_gradient_descent(
     #     [(-5, 1), (5, 1), (0, 0), (0, 2), (-3, 0.2), (3, 0.2), (-3, 1.8), (3, 1.8), (1, 0.02), (-1, 0.02), (1, 1.98),
     #      (-1, 1.98)])
     # print(e)
 
-    # e = centered_ellipse_from_gradient_descent(
-    #     [
-    #         (0.993, 1.002),
-    #         (-0.993, -1.002),
-    #         (0, -0.141),
-    #         (0, 0.141),
-    #         (0.141, 0),
-    #         (-0.141, 0),
-    #     ]
-    # )
-    # print(e)
-    # x, y = e.samples()
-    # plt.plot(x, y, linestyle='None', marker='o')
+    e = centered_ellipse_from_gradient_descent(
+        [
+            (0.993, 1.002),
+            (-0.993, -1.002),
+            (0, -0.141),
+            (0, 0.141),
+            (0.141, 0),
+            (-0.141, 0),
+        ]
+    )
+    print(e)
+    x, y = e.samples()
+    plt.plot(x, y, linestyle='None', marker='o')
 
-    I0 = cv2.imread("../../data/video4-frame1/00001_000.png")[:500, :500, 0].astype(np.float32) / 255
-    I45 = cv2.imread("../../data/video4-frame1/00001_045.png")[:500, :500, 0].astype(np.float32) / 255
-    I90 = cv2.imread("../../data/video4-frame1/00001_090.png")[:500, :500, 0].astype(np.float32) / 255
-    I135 = cv2.imread("../../data/video4-frame1/00001_135.png")[:500, :500, 0].astype(np.float32) / 255
-
-    ellipses = make_elipse_image(I0, I45, I90, I135)
-    im = render_ellipse(ellipses, 0)
-    plt.imshow(im, cmap='gray')
-    plt.show()
-    im = render_ellipse(ellipses, np.pi / 2)
-    plt.imshow(im, cmap='gray')
-    plt.show()
+    # I0 = cv2.imread("../../data/video4-frame1/00001_000.png")[:500, :500, 0].astype(np.float32) / 255
+    # I45 = cv2.imread("../../data/video4-frame1/00001_045.png")[:500, :500, 0].astype(np.float32) / 255
+    # I90 = cv2.imread("../../data/video4-frame1/00001_090.png")[:500, :500, 0].astype(np.float32) / 255
+    # I135 = cv2.imread("../../data/video4-frame1/00001_135.png")[:500, :500, 0].astype(np.float32) / 255
+    #
+    # ellipses = make_elipse_image(I0, I45, I90, I135)
+    # im = render_ellipse(ellipses, 0)
+    # plt.imshow(im, cmap='gray')
+    # plt.show()
+    # im = render_ellipse(ellipses, np.pi / 2)
+    # plt.imshow(im, cmap='gray')
+    # plt.show()
 
     e = ellipse_from_stokes_vector((1, 0.01, 0.99, 0))
     print(e)
