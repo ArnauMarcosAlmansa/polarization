566179b9ac796125b029fe4b5ac2115bf1f9ee74
Arnau Marcos Almansa <arnaumarcosalmansa@gmail.com>
2024-08-01 18:46:23 +0200

    generar rayos durante la preparacion

--- src/scripts/prepare_dataset.py
+++ src/scripts/prepare_dataset.py
@@ -12,6 +12,7 @@ from src.load_polarimetric import PolarimetricImage
 from src.pipeline import Pipeline, sequential, command, function
 import os
 
+
 def extract_frames(videos_dir, frames_dir):
     tasks = [function(lambda: os.makedirs(frames_dir, exist_ok=True))]
     for filename in os.listdir(videos_dir):
@@ -20,9 +21,9 @@ def extract_frames(videos_dir, frames_dir):
     return tasks
 
 
-
 def laplacian_var(im):
     return cv2.Laplacian(im, cv2.CV_64F).var()
+
 
 def remove_blurry_frames(frames_dir, threshold):
     laplacian_vars = OrderedDict()
@@ -43,24 +44,34 @@ def train_test_split(samples, test=0.2):
     return samples[boundary:], samples[:boundary]
 
 
-
 def split_transforms(transforms_path, transforms_dir):
     os.makedirs(transforms_dir, exist_ok=True)
     with open(transforms_path) as f:
         transforms = json.load(f)
 
     frame_paths = [filename for filename in map(lambda frame: frame["file_path"], transforms["frames"])]
+
     def clean_filename(fn):
         fn = os.path.basename(fn)
         fn_no_ext = ".".join(fn.split(".")[:-1])
         fn_no_idx = "_".join(fn_no_ext.split("_")[:-1])
         return fn_no_idx
+
+    relative = os.path.relpath(os.path.dirname(transforms_path), transforms_dir)
+
+    def update_file_path(frame):
+        frame["file_path"] = os.path.join(relative, frame["file_path"])
+        return frame
+
+    transforms["frames"] = list(map(update_file_path, transforms["frames"]))
+
     video_names = list(set(map(clean_filename, frame_paths)))
     for video_name in video_names:
         transforms_for_video = copy.deepcopy(transforms)
-        poses = [frame for frame in transforms_for_video["frames"] if "_".join(frame["file_path"].split("/")[-1].split("_")[:-1]) == video_name]
+        frames = [frame for frame in transforms_for_video["frames"] if
+                 "_".join(frame["file_path"].split("/")[-1].split("_")[:-1]) == video_name]
 
-        poses_train, poses_test = train_test_split(poses, test=0.2)
+        poses_train, poses_test = train_test_split(frames, test=0.2)
 
         transforms_for_video["frames"] = poses_train
         with open(os.path.join(transforms_dir, f"{video_name}_train_transforms.json"), "w") as f:
@@ -83,7 +94,7 @@ def generate_rays(transforms_dir, rays_dir):
             ray_generator = RayGenerator(transforms)
             for frame in transforms["frames"]:
                 pose = np.array(frame["transform_matrix"])
-                image = PolarimetricImage.load(frame["file_path"])
+                image = PolarimetricImage.load(os.path.join(transforms_dir, frame["file_path"]))
                 ir0, ir45, ir90, ir135 = ray_generator.get_rays_for_pose_and_image(pose, image)
                 bytes_ir0 = bytearray(ir0.to_raw_data())
                 bytes_ir45 = bytearray(ir45.to_raw_data())
@@ -99,6 +110,7 @@ def train_nerfs():
     tasks = []
 
     return tasks
+
 
 if __name__ == '__main__':
     videos_dir = "/home/amarcos/workspace/polarization/data/grapadora-videos/polarimetric"
@@ -117,15 +129,19 @@ if __name__ == '__main__':
 
     run_colmap_task = sequential([
         function(lambda: os.makedirs(colmap_dir, exist_ok=True)),
-        command(f"QT_QPA_PLATFORM=offscreen colmap feature_extractor --database_path {colmap_dir}/database.db --image_path {frames_dir} --ImageReader.single_camera_per_folder 1"),
+        command(
+            f"QT_QPA_PLATFORM=offscreen colmap feature_extractor --database_path {colmap_dir}/database.db --image_path {frames_dir} --ImageReader.single_camera_per_folder 1"),
         command(f"QT_QPA_PLATFORM=offscreen colmap exhaustive_matcher --database_path {colmap_dir}/database.db"),
         command(f"mkdir {colmap_dir}/sparse"),
-        command(f"QT_QPA_PLATFORM=offscreen colmap mapper --database_path {colmap_dir}/database.db --image_path {frames_dir} --output_path {colmap_dir}/sparse"),
-        command(f"colmap model_converter --input_path {colmap_dir}/sparse/0 --output_path {colmap_dir}/sparse/0 --output_type TXT"),
+        command(
+            f"QT_QPA_PLATFORM=offscreen colmap mapper --database_path {colmap_dir}/database.db --image_path {frames_dir} --output_path {colmap_dir}/sparse"),
+        command(
+            f"colmap model_converter --input_path {colmap_dir}/sparse/0 --output_path {colmap_dir}/sparse/0 --output_type TXT"),
     ])
 
     colmap2nerf = sequential([
-        command(f"python3 {this_fle_path}/colmap2nerf.py --text {colmap_dir}/sparse/0 --images {frames_dir} --out {colmap_dir}/transforms.json"),
+        command(
+            f"python3 {this_fle_path}/colmap2nerf.py --text {colmap_dir}/sparse/0 --images {frames_dir} --out {colmap_dir}/transforms.json"),
     ])
 
     split_transforms_task = function(lambda: split_transforms(f"{colmap_dir}/transforms.json", transforms_dir))
@@ -145,4 +161,3 @@ if __name__ == '__main__':
     ])
 
     pipeline.run()
-
