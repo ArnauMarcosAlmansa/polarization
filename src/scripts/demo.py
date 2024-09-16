fdcbcdc78d56ce6801cdf79de4cae6f472f46487
Arnau Marcos Almansa <arnaumarcosalmansa@gmail.com>
2024-09-16 19:04:32 +0200

    compute mueller matrix for every pixel in an image

--- src/scripts/demo.py
+++ src/scripts/demo.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 import argparse
 import copy
 import json
+import math
 import os
 import sys
 from dataclasses import dataclass
+from itertools import pairwise
 from typing import Any
 
 import matplotlib.pyplot as plt
@@ -15,6 +17,8 @@ import torch
 from src.config.config import Config
 from src.data_generation.RayGenerator import RayGenerator
 from src.device import device
+from src.scripts.solve_mueller_matrices import image_4channel_to_stokes, solve_muellers_for_image, \
+    make_mueller_image_validity_map, make_stokes_mask
 from src.training import CRANeRFModel
 
 
@@ -23,6 +27,7 @@ def parse_args():
     argparser.add_argument("--config", default="../../config.yaml")
     return argparser.parse_args()
 
+
 def load_last_available_checkpoint_for_model(model: CRANeRFModel, nerfs_dir: str, model_name: str):
     checkpoints = os.listdir(f"{nerfs_dir}/{model_name}")
     if not checkpoints:
@@ -30,13 +35,13 @@ def load_last_available_checkpoint_for_model(model: CRANeRFModel, nerfs_dir: str
         print(f"No checkpoints available for '{model_name}'")
         return (model, 1)
 
-
     checkpoints = sorted(checkpoints)
     checkpoint = checkpoints[-1]
     model.load(f"{nerfs_dir}/{model_name}/{checkpoint}")
     start = int(checkpoint[:10])
 
     return (model, start)
+
 
 def camera_transform_to_pose(transform: list[list[float]]) -> np.ndarray:
     return np.array(transform)
@@ -47,16 +52,18 @@ class Int2:
     x: int
     y: int
 
+
 def scaled_grid_coordinates(pos: Int2, grid: np.ndarray, maximums: Int2):
     pos = copy.copy(pos)
     while maximums.x > grid.shape[1]:
-        maximums.x //=2
+        maximums.x //= 2
         maximums.y //= 2
 
-        pos.x //=2
+        pos.x //= 2
         pos.y //= 2
 
     return pos
+
 
 @dataclass
 class Stokes:
@@ -73,6 +80,7 @@ class Stokes:
 
     def numpy(self) -> np.ndarray:
         return np.array([self.s0, self.s1, self.s2])
+
 
 def solve_mueller(Sout_000, Sout_045, Sout_090, Sout_135, Sneutral, Sin_000, Sin_045, Sin_090, Sin_135):
     Sout_000 = Sout_000 - Sneutral
@@ -102,6 +110,7 @@ def solve_mueller(Sout_000, Sout_045, Sout_090, Sout_135, Sneutral, Sin_000, Sin
 
     return M
 
+
 INPUT_STOKES = [
     Stokes(1, 1, 0),
     Stokes(1, 0, 1),
@@ -125,16 +134,25 @@ class Demo:
         self.last_image = np.zeros((1024, 1224), dtype=np.uint8)
         self.last_surface = pygame.surfarray.make_surface(self.last_image)
         self.last_rays = np.zeros((1, 1))
-        self.camera_pose = camera_transform_to_pose(self.transforms.lighted.test["frames"][0]["transform_matrix"])
-        self.render(self.camera_pose)
+        self.camera_pose = camera_transform_to_pose(self.transforms.lighted_135.train["frames"][0]["transform_matrix"])
+        # self.render(self.models.lighted, self.camera_pose)
         # self.render_max_difference(self.camera_pose)
-        self.compute_mueller_matrix_for_pos(500, 500)
+        # self.compute_mueller_matrix_for_point(500, 500)
+        # self.ray_generator._verify_ray_rotations(self.camera_pose)
+        # pol_im = self.render_polarimetric(self.models.lighted_135, self.camera_pose)
+        #
+        # plt.imsave("test_000.png", pol_im[:, :, 0], cmap="gray", vmin=0.0, vmax=1.0)
+        # plt.imsave("test_045.png", pol_im[:, :, 1], cmap="gray", vmin=0.0, vmax=1.0)
+        # plt.imsave("test_090.png", pol_im[:, :, 2], cmap="gray", vmin=0.0, vmax=1.0)
+        # plt.imsave("test_135.png", pol_im[:, :, 3], cmap="gray", vmin=0.0, vmax=1.0)
+        self.compute_mueller_matrix_for_entire_image()
+        print("DONE")
 
     def step(self):
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
-                self.compute_mueller_matrix_for_pos(pos[0], pos[1])
+                self.compute_mueller_matrix_for_point(pos[0], pos[1])
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
@@ -144,13 +162,15 @@ class Demo:
         pygame.display.flip()
 
     @torch.no_grad()
-    def render_max_difference(self, camera_pose):
+    def render_max_difference(self, model: CRANeRFModel, camera_pose):
         rays_per_polariation = self.ray_generator.get_rays_for_pose(camera_pose)
 
         images = []
         for rays in rays_per_polariation:
-            image_rays = torch.from_numpy(np.dstack(rays).reshape((int(self.ray_generator.h * self.ray_generator.w), 12)).astype(np.float32)).to(device)
-            image = self.render_rays(image_rays).reshape((int(self.ray_generator.h), int(self.ray_generator.w)))
+            image_rays = torch.from_numpy(
+                np.dstack(rays).reshape((int(self.ray_generator.h * self.ray_generator.w), 12)).astype(np.float32)).to(
+                device)
+            image = self.render_rays(model, image_rays).reshape((int(self.ray_generator.h), int(self.ray_generator.w)))
             images.append(image)
 
         stack = np.dstack(images)
@@ -158,37 +178,54 @@ class Demo:
         minimums = stack.min(2)
 
         self.last_image = ((maximums - minimums) * 255).astype(np.uint8).transpose()
-        self.last_surface = pygame.surfarray.make_surface(np.dstack([self.last_image, self.last_image, self.last_image]))
+        self.last_surface = pygame.surfarray.make_surface(
+            np.dstack([self.last_image, self.last_image, self.last_image]))
         self.last_surface = pygame.transform.scale(self.last_surface, (1224, 1024))
 
-    def render_rays(self, image_rays):
+    def render_rays(self, model: CRANeRFModel, image_rays):
         resulting_image = np.zeros((int(self.ray_generator.h * self.ray_generator.w)))
 
         batch_start = 0
         batch_end = 0
         for ray_batch in image_rays.split(2048, 0):
             batch_end += ray_batch.shape[0]
-            ret = self.models.lighted.render_rays(ray_batch)
+            ret = model.render_rays(ray_batch)
             resulting_image[batch_start:batch_end] = ret["rgb_map"].cpu().numpy().squeeze(1)
             batch_start += ray_batch.shape[0]
 
         return resulting_image
 
     @torch.no_grad()
-    def render(self, camera_pose):
+    def render_polarimetric(self, model: CRANeRFModel, camera_pose):
+        rays_per_polariation = self.ray_generator.get_rays_for_pose(camera_pose)
+        images = []
+        for rays in rays_per_polariation:
+            image_rays = torch.from_numpy(
+                np.dstack(rays).reshape((int(self.ray_generator.h * self.ray_generator.w), 12)).astype(np.float32)).to(
+                device)
+            image = self.render_rays(model, image_rays).reshape((int(self.ray_generator.h), int(self.ray_generator.w)))
+            images.append(image)
+
+        polarimetric_image = np.dstack(images)
+        return polarimetric_image
+
+    @torch.no_grad()
+    def render(self, model: CRANeRFModel, camera_pose):
         r_o, r_d, r_u, r_r = self.ray_generator.get_nonrotated_rays_for_pose(camera_pose)
         self.last_rays = np.dstack([r_o, r_d, r_u, r_r]).astype(np.float32)
-        image_rays = torch.from_numpy(self.last_rays.reshape((int(self.ray_generator.h * self.ray_generator.w), 12))).to(device)
+        image_rays = torch.from_numpy(
+            self.last_rays.reshape((int(self.ray_generator.h * self.ray_generator.w), 12))).to(device)
 
-        resulting_image = self.render_rays(image_rays)
+        resulting_image = self.render_rays(model, image_rays)
 
         self.last_image = resulting_image.reshape((int(self.ray_generator.h), int(self.ray_generator.w)))
         self.last_image = (self.last_image * 255).astype(np.uint8).transpose()
-        self.last_surface = pygame.surfarray.make_surface(np.dstack([self.last_image, self.last_image, self.last_image]))
+        self.last_surface = pygame.surfarray.make_surface(
+            np.dstack([self.last_image, self.last_image, self.last_image]))
         self.last_surface = pygame.transform.scale(self.last_surface, (1224, 1024))
 
     @torch.no_grad()
-    def compute_mueller_matrix_for_pos(self, mouse_x, mouse_y):
+    def compute_mueller_matrix_for_point(self, mouse_x, mouse_y):
         pos = scaled_grid_coordinates(Int2(mouse_x, mouse_y), self.last_rays, Int2(1224, 1024))
         rays = self.ray_generator.get_rays_for_pose(self.camera_pose)
         ray_000 = rays[0][0][pos.y, pos.x], rays[0][1][pos.y, pos.x], rays[0][2][pos.y, pos.x], rays[0][3][pos.y, pos.x]
@@ -226,11 +263,16 @@ class Demo:
         lighted_135_090 = self.render_ray_for_model(self.models.lighted_135, ray_090)
         lighted_135_135 = self.render_ray_for_model(self.models.lighted_135, ray_135)
 
-        stokes_neutral = Stokes.from_intensities(neutral_000.item(), neutral_045.item(), neutral_090.item(), neutral_135.item())
-        stokes_000 = Stokes.from_intensities(lighted_000_000.item(), lighted_000_045.item(), lighted_000_090.item(), lighted_000_135.item())
-        stokes_045 = Stokes.from_intensities(lighted_045_000.item(), lighted_045_045.item(), lighted_045_090.item(), lighted_045_135.item())
-        stokes_090 = Stokes.from_intensities(lighted_090_000.item(), lighted_090_045.item(), lighted_090_090.item(), lighted_090_135.item())
-        stokes_135 = Stokes.from_intensities(lighted_135_000.item(), lighted_135_045.item(), lighted_135_090.item(), lighted_135_135.item())
+        stokes_neutral = Stokes.from_intensities(neutral_000.item(), neutral_045.item(), neutral_090.item(),
+                                                 neutral_135.item())
+        stokes_000 = Stokes.from_intensities(lighted_000_000.item(), lighted_000_045.item(), lighted_000_090.item(),
+                                             lighted_000_135.item())
+        stokes_045 = Stokes.from_intensities(lighted_045_000.item(), lighted_045_045.item(), lighted_045_090.item(),
+                                             lighted_045_135.item())
+        stokes_090 = Stokes.from_intensities(lighted_090_000.item(), lighted_090_045.item(), lighted_090_090.item(),
+                                             lighted_090_135.item())
+        stokes_135 = Stokes.from_intensities(lighted_135_000.item(), lighted_135_045.item(), lighted_135_090.item(),
+                                             lighted_135_135.item())
 
         M = solve_mueller(
             stokes_000.numpy(),
@@ -255,6 +297,28 @@ class Demo:
         ret = model.render_rays(ray_batch)
         return ret["rgb_map"][0, 0]
 
+    @torch.no_grad()
+    def compute_mueller_matrix_for_entire_image(self):
+        neutral_pol_im = self.render_polarimetric(self.models.neutral, self.camera_pose)
+        lighted_000_pol_im = self.render_polarimetric(self.models.lighted_000, self.camera_pose) - neutral_pol_im
+        lighted_045_pol_im = self.render_polarimetric(self.models.lighted_045, self.camera_pose) - neutral_pol_im
+        lighted_090_pol_im = self.render_polarimetric(self.models.lighted_090, self.camera_pose) - neutral_pol_im
+        lighted_135_pol_im = self.render_polarimetric(self.models.lighted_135, self.camera_pose) - neutral_pol_im
+
+        # neutral_stokes = image_4channel_to_stokes(neutral_pol_im)
+        lighted_000_stokes = image_4channel_to_stokes(lighted_000_pol_im)
+        lighted_045_stokes = image_4channel_to_stokes(lighted_045_pol_im)
+        lighted_090_stokes = image_4channel_to_stokes(lighted_090_pol_im)
+        lighted_135_stokes = image_4channel_to_stokes(lighted_135_pol_im)
+
+        total_mask = make_stokes_mask(lighted_000_stokes) & make_stokes_mask(lighted_045_stokes) & make_stokes_mask(lighted_090_stokes) & make_stokes_mask(lighted_135_stokes)
+
+        mueller_image = solve_muellers_for_image(lighted_000_stokes, lighted_045_stokes, lighted_090_stokes, lighted_135_stokes, total_mask)
+
+        validity_map = make_mueller_image_validity_map(mueller_image)
+
+        plt.imshow(validity_map),
+        plt.show()
 
 
 @dataclass
@@ -266,8 +330,10 @@ class ModelNames:
     lighted_090: str
     lighted_135: str
 
+
 class Models:
-    def __init__(self, neutral: CRANeRFModel, lighted: CRANeRFModel, lighted_000: CRANeRFModel, lighted_045: CRANeRFModel, lighted_090: CRANeRFModel, lighted_135: CRANeRFModel):
+    def __init__(self, neutral: CRANeRFModel, lighted: CRANeRFModel, lighted_000: CRANeRFModel,
+                 lighted_045: CRANeRFModel, lighted_090: CRANeRFModel, lighted_135: CRANeRFModel):
         self.neutral = neutral
         self.lighted = lighted
         self.lighted_000 = lighted_000
@@ -287,6 +353,7 @@ class Models:
 
         return Models(neutral[0], lighted[0], lighted_000[0], lighted_045[0], lighted_090[0], lighted_135[0])
 
+
 @dataclass
 class TransformPair:
     train: dict[str, Any]
@@ -296,6 +363,7 @@ class TransformPair:
 def load_transform(filename: str) -> dict[str, Any]:
     with open(filename, "rt") as f:
         return json.load(f)
+
 
 @dataclass
 class ModelTransforms:
@@ -336,7 +404,6 @@ class ModelTransforms:
                 test=load_transform(f"{transforms_dir}/{names.lighted_135}_test_transforms.json"),
             ),
         )
-
 
 
 if __name__ == '__main__':
